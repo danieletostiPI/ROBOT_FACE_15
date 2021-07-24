193c00b8b87d23d267c7851055f160b48116eeab
danieletostiPI <tosti979@gmail.com>
2021-07-24 13:59:50 +0200

    dajee

--- main.py
+++ main.py
@@ -150,7 +150,7 @@ if __name__ == "__main__":
                 else:
                     inc = 1
                 dc1 = rotate_right(dc1, inc)
-                time.sleep(inc * 0.01)
+                time.sleep(inc * 0.1)
                 print("Increase right %d", inc)
             if dc1 <= 9:
                 enabler = 0
@@ -168,7 +168,7 @@ if __name__ == "__main__":
                 else:
                     inc = 1
                 dc1 = rotate_left(dc1, inc)
-                time.sleep(inc * 0.01)
+                time.sleep(inc * 0.1)
                 print("Increase left %d", inc)
             if dc1 >= 42:
                 enablel = 0
