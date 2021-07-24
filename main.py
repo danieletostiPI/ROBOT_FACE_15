deaa953f895360bf730128246da530a53f28fbee
danieletostiPI <tosti979@gmail.com>
2021-07-24 11:15:20 +0200

    dajee

--- main.py
+++ main.py
@@ -50,8 +50,8 @@ stop = True
 
 if __name__ == "__main__":
     cap = cv2.VideoCapture(0)
-    cap.set(3, 1280)
-    cap.set(4, 720)
+    cap.set(3, 960)
+    cap.set(4, 540)
 
     while go:
         # --------------------------
